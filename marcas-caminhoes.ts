67cffcc8964bbca25896c9489c4669ce166ed402
ronaldodalan <ronaldodalan@gmail.com>
2026-04-01 16:03:06 -0300

    01-04-2026

---
+++ marcas-caminhoes.ts
@@ -0,0 +1,28 @@
+
+
+// Lista de caminhões da marca DAF
+
+
+
+const ListaDaDAF = {
+  cf: {
+    nome: 'CF',
+    tipo: ['box', 'flatbed', 'dump', 'tank', 'concrete_mixer', 'grain', 'skip_loader'],
+    cores: ['Branco', 'Preto', 'Prata', 'Azul', 'Vermelho'],
+    png: [],
+  },
+
+  lf: {
+    nome: 'LF',
+    tipo: ['box', 'flatbed', 'skip_loader'],
+    cores: ['Branco', 'Prata', 'Azul', 'Vermelho'],
+    png: [],
+  },
+
+  xf: {
+    nome: 'XF',
+    tipo: ['box', 'flatbed', 'tank', 'grain', 'car_carrier'],
+    cores: ['Branco', 'Preto', 'Prata', 'Azul', 'Vermelho'],
+    png: [],
+  }
+}
